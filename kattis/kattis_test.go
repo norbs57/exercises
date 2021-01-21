de47209c04d8da2a5af41befb6bf8804125f5106
Norbs <norvaer@gmail.com>
2021-01-21 23:00:46 +0000

    Add files via upload

--- kattis/kattis_test.go
+++ kattis/kattis_test.go
@@ -12,7 +12,7 @@ func TestKattis(t *testing.T) {
 	tsps := []string{"10", "52", "280", "1000", "1173"}
 	costs := []int{331, 8708, 3132, 24556237, 72059}
 	for i, s := range tsps {
-		fName := "../data/tsp" + s + ".txt"
+		fName := "./data/tsp" + s + ".txt"
 		f, err := os.Open(fName)
 		if err == nil {
 			os.Stdin = f
